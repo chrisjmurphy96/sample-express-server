b37223666efab9d93b432d58b32e6efe589b3f20
chrisjmurphy96 <chrisjmurphy96@gmail.com>
2019-07-01 19:56:05 -0500

    Added disable x-powered-by and catch-all route

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get('/myPage', function (req, res) {
-  res.sendFile(`${__dirname}/index.html`);
+const config = {
+    port: process.env.APP_PORT || '3000'
+}
+
+/*
+ * Minimum security recommendation by Express.
+ * x-powered-by exposes that you are using Express, which can
+ * lead to targeted exploits.
+ */  
+app.disable('x-powered-by');
+
+app.get('/myPage', function (request, response) {
+    response.sendFile(`${__dirname}/index.html`);
+});
+
+// Catch all other traffic and send a 404 Not Found
+app.all('*', function(request, response) {
+    response.sendStatus(404);
 });
 
 app.listen(3000);
+
+console.log(`Sample express server running on port ${config.port}`);
